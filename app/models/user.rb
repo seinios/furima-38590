988875b98989dd34857f98a83b50848ef687b2bd
seinios <kanissan.sao@gmail.com>
2022-11-04 17:40:58 +0900

    ユーザーモデル単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,10 @@ class User < ApplicationRecord
 
   has_many :items
   has_many :consumers
+
+  validates :family_name,            presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana,       presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name,             presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kana,        presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   
 end
