f41d7c9ec3413284f86aad44b3180a6008da4ca8
seinios <kanissan.sao@gmail.com>
2022-11-10 17:38:10 +0900

    モデル単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,9 @@ class Item < ApplicationRecord
   belongs_to :address
   belongs_to :period
 
-  validates :name,:explanation,:image, presence: true
+  validates :image, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :explanation, presence: true, length: { maximum: 1000 }
 
   validates :price,numericality: { only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9_999_999},presence: { messasge: "can't be blank"}
   validates :category_id,:situation_id,:delivery_charge_id,:address_id,:period_id, numericality: { other_than: 1 , message: "can't be blank"}
