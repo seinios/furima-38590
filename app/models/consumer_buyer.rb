2038df52fb72203151fa97abf1fc04edf7011d2f
seinios <kanissan.sao@gmail.com>
2022-11-22 15:12:01 +0900

    バリデーション修正

--- app/models/consumer_buyer.rb
+++ app/models/consumer_buyer.rb
@@ -2,15 +2,18 @@ class ConsumerBuyer
   include ActiveModel::Model
   attr_accessor :post_code,:address_id,:municipality,:address_number,:building,:tel,:user_id,:item_id,:token
 
-  validates :post_code, format: { with: /[-]/ },presence: true
-  validates :address_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :municipality,:address_number,:tel,presence: true
-  validates :tel, numericality: { only_integer: true, message: 'is invalid.Input only number' }
-  validates :tel, length: { maximum: 11, message: "is too long" }
-  validates :tel, length: { minimum: 10, message: "is too short" }
-  validates :token,presence: true
+  with_options presence: true do
+    validates :post_code, format:  { with: /\A\d{3}-\d{4}\z/ }
+    validates :municipality,:address_number,:tel
+    validates :tel, numericality: { only_integer: true, message: 'is invalid.Input only number' }
+    validates :tel, length: { maximum: 11, message: "is too long" }
+    validates :tel, length: { minimum: 10, message: "is too short" }
+    validates :token
+    validates :user_id,:item_id
+  end
 
-  validates :user_id,:item_id,presence:true
+  validates :address_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 
   def save
 
