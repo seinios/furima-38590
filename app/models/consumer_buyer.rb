7309519c69e9bbf7fc5a31d18597a3ec63787462
seinios <kanissan.sao@gmail.com>
2022-11-23 14:43:01 +0900

    エラーメッセージ日本語化

--- app/models/consumer_buyer.rb
+++ app/models/consumer_buyer.rb
@@ -11,7 +11,7 @@ class ConsumerBuyer
   end
 
 
-  validates :address_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :address_id, numericality: { other_than: 1 , message: "を入力してください"}
 
 
   def save
--- app/models/item.rb
+++ app/models/item.rb
@@ -17,5 +17,5 @@ class Item < ApplicationRecord
   validates :explanation, presence: true, length: { maximum: 1000 }
 
   validates :price,numericality: { only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9_999_999},presence: { messasge: "can't be blank"}
-  validates :category_id,:situation_id,:delivery_charge_id,:address_id,:period_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id,:situation_id,:delivery_charge_id,:address_id,:period_id, numericality: { other_than: 1 , message: "を入力してください"}
 end
